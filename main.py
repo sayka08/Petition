99550c23b675873aabded75e106843591c3ad2dc
sayka08 <s.serik@ovtech.kz>
2024-11-11 18:11:47 +0600

    Fixed filter limit

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ async def create_vote(
 @app.get("/petitions/", response_model=List[PetitionResponse])
 async def get_petitions(
         skip: int = Query(0, alias="page", ge=0),
-        limit: int = Query(10, le=100),
+        limit: int = Query(2, le=100),
         search: str = Query(None, max_length=50),
         sort_by: str = Query("created_at", regex="^(created_at|votes_count)$"),
         sort_order: str = Query("desc", regex="^(asc|desc)$"),
